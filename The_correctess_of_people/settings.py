4eed24aa4d5c5dc6ede2e6ecfc88c52604ea9788
alibeh18 <alibehrouzifh@gmail.com>
2025-08-10 22:31:46 +0330

    Connected Django to PostgreSQL

--- The_correctess_of_people/settings.py
+++ The_correctess_of_people/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'The_correctess_of_people.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydjango_db',
+        'USER': 'django_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
